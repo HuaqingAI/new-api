c665d24d617d581a2e7b509d89d0e40c9b091de6
kongsiyu <boil@vip.qq.com>
2026-06-03 10:04:20 +0800

    fix agentplatform postgres migration relation

--- model/agentplatform/resource_version.go
+++ model/agentplatform/resource_version.go
@@ -23,7 +23,7 @@ type ResourceVersion struct {
 	PublishedAt     *time.Time `json:"published_at"`
 	CreatedAt       time.Time  `json:"created_at"`
 	UpdatedAt       time.Time  `json:"updated_at"`
-	Resource        Resource   `json:"-" gorm:"foreignKey:ResourceId;references:ResourceId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Resource        Resource   `json:"-" gorm:"references:ResourceId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 func (ResourceVersion) TableName() string {
